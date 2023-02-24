357333cfa085522ba84f39255fae2dc76e28904e
lynch <lynch@David-Lynch>
2023-02-24 11:26:01 -0500

    Initial GUI template and, main and database

---
+++ BlackHardSweetCompany/src/DatabaseManager/DatabaseController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DatabaseManager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author lynch
+ */
+public class DatabaseController {
+   static final String DB_URL = "jdbc:mysql://localhost/";
+   static final String USER = "admin";
+   static final String PASS = "admin123";
+   
+   
+}
---
+++ BlackHardSweetCompany/src/blackhardsweetcompany/BlackHardSweetCompany.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package blackhardsweetcompany;
+
+import GUI.LoaderFrame;
+import DatabaseManager.DatabaseController;
+
+/**
+ *
+ * @author lynch
+ */
+public class BlackHardSweetCompany {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new LoaderFrame().setVisible(true);
+//       DatabaseController databaseControl = new DatabaseController();
+       
+    }   
+}
