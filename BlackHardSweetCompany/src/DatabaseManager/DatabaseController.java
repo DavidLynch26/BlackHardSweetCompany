f1cb1ea8cc171aa931b2bad789b716745e72bf2e
lynch <lynch@David-Lynch>
2023-02-24 21:58:03 -0500

    Database and tables are successfully made, initial data is needed for: Admin, Driver, and inventory.

--- BlackHardSweetCompany/src/DatabaseManager/DatabaseController.java
+++ BlackHardSweetCompany/src/DatabaseManager/DatabaseController.java
@@ -14,8 +14,39 @@ import java.sql.Statement;
  * @author lynch
  */
 public class DatabaseController {
-    private static final String[] tableNames = {"Driver", "Customer", "Admin", "Inventory"};
-    private static final String DB_URL = "jdbc:mysql://localhost/";
+    private static final String[] baseData= {"(id INTEGER not null,"
+            + "firstname VARCHAR(255),"
+            + "lastname VARCHAR(255),"
+            + "username VARCHAR(255),"
+            + "password VARCHAR(255),"
+            + "PRIMARY KEY(id))",
+        
+            "(id INTEGER not null,"
+            + "firstname VARCHAR(255),"
+            + "lastname VARCHAR(255),"
+            + "username VARCHAR(255),"
+            + "password VARCHAR(255),"
+            + "PRIMARY KEY(id))",
+            
+            "(id INTEGER not null,"
+            + "firstname VARCHAR(255),"
+            + "lastname VARCHAR(255),"
+            + "address VARCHAR(255),"
+            + "PRIMARY KEY(id))",
+            
+            "(id VARCHAR(255),"
+            + "item VARCHAR(255),"
+            + "price DECIMAL(65),"
+            + "PRIMARY KEY(id))", 
+            
+            "(id INTEGER not null ,"
+            + "itemid VARCHAR(255),"
+            + "custid INTEGER,"
+            + "price DECIMAL(65),"
+            + "PRIMARY KEY(id))"};
+    
+    private static final String[] tableNames = {"Admin", "Driver", "Customer", "Inventory", "CustOrder"};
+    private static final String DB_URL = "jdbc:mysql://localhost/" + getDB_NAME();
     private static final String USER = "root";
     private static final String PASS = "";
     private static final String DB_NAME = "BlackHardSweetCompany";
@@ -26,24 +57,46 @@ public class DatabaseController {
 
     private void init(){
         Statement stmt = setConnection();
+//        drop(tableNames, "TABLE ");
         createDatabase(stmt);
- //       createTables(stmt);
+        createTables(stmt);
     }
 
-    public void createTbles(Statement stmt){
-
- //       stmt.close();
+    private void createTables(Statement stmt){
+        Integer count = 0;
+        for(String table: getTableNames()){
+            try {
+                String sql = "CREATE TABLE " + table + baseData[count];
+//                System.out.println(sql);
+                stmt.executeUpdate(sql);
+                System.out.println(table + " created successfully...");
+                
+            } catch (SQLException e) {
+                if(e.getErrorCode() == 1050){
+                    System.out.println(e.getMessage());
+                }else{
+                    e.printStackTrace();
+                }              
+            }
+            count++;
+        }
     }
 
-    private void drop(){
-        try {
-            Statement stmt = setConnection();
-            String sql = "Drop Database " + getDB_NAME();
-            stmt.executeUpdate(sql);
-            System.out.println(getDB_NAME()+ " dropped successfully...");  
-            stmt.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+    private void drop(String[] names, String type){
+        for(String name: names){
+            try {
+                Statement stmt = setConnection();
+                String sql = "Drop " + type + name;
+                stmt.executeUpdate(sql);
+                System.out.println(name + " dropped successfully...");  
+                stmt.close();
+            } catch (SQLException e) {
+                if(e.getErrorCode() == 3702){
+                    System.out.println(e.getMessage());
+                }else{
+                    e.printStackTrace();
+                }        
+            }
         }
     }
 
@@ -74,19 +127,22 @@ public class DatabaseController {
             }
         }
     }
-     public static String getDB_URL() {
-         return DB_URL;
-     }
+    public static String getDB_URL() {
+        return DB_URL;
+    }
 
-     public static String getUSER() {
-         return USER;
-     }
+    public static String getUSER() {
+        return USER;
+    }
 
-     public static String getPASS() {
-         return PASS;
-     }
+    public static String getPASS() {
+        return PASS;
+    }
 
-     public static String getDB_NAME() {
-         return DB_NAME;
-     }
+    public static String getDB_NAME() {
+        return DB_NAME;
+    }
+    public static String[] getTableNames() {
+       return tableNames;
+    }
 }
